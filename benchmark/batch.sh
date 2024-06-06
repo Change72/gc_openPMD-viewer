b5d0fb022a87980d1b22e6adcc6d599f557bb197
Chang Guo <cguo51@en4228271l.fulton.ad.asu.edu>
2024-06-06 13:27:18 -0700

    update benchmark result

--- benchmark/batch.sh
+++ benchmark/batch.sh
@@ -6,11 +6,17 @@ if [ $# -eq 0 ]; then
     bpfile="/data/gc/rocksdb-index/WarpX/build/bin/diags/diag2"
     index="/data/gc/rocksdb-index/GEOSIndex/cmake-build-debug/diag2"
     iteration=500
+    species="electrons"
+    total_particle_num=41405256
+    output_file="results/10g_iteration_500/benchmark_result_00001_to_01.csv"
 else
     # Use the value provided as a command-line argument
     bpfile=$1
     index=$2
     iteration=$3
+    species=$4
+    total_particle_num=$5
+    output_file=$6
 fi
 
 # Loop 1000 times
@@ -20,19 +26,19 @@ for ((i=1; i<=10000; i++)); do
         --bpfile $bpfile \
         --index $index \
         --iteration $iteration \
-	    --species "hydrogen" \
-        --select_set "x,y,z;ux,uy,uz;x,y,z,ux,uy,uz" \
+	    --species $species \
+        --select_set "x,y;ux,uy;x,y,ux,uy;x,ux,uy,uz;x,y,ux,uy,uz" \
         --threshold 0.01 \
-	    --percentage_range 0.0001 \
-        --total_particle_num 13122018434 \
+	    --percentage_range 0.0001,0.1,10 \
+        --total_particle_num $total_particle_num \
         --limit_block_num 500 \
-        --output_file "results/500g_iteration_10000/benchmark_result_00001_to_01.csv" \
+        --output_file $output_file \
         > "log/benchmark_result_$(printf '%05d' $i).log" \
         2>&1 &
-    sleep 1200
+    sleep 120
     # if i / 20 == 0, Sleep for 3600 seconds
-    if [ $((i % 2)) -eq 0 ]; then
-        sleep 1800
-    fi
+    # if [ $((i % 2)) -eq 0 ]; then
+    #     sleep 1800
+    # fi
 done
 
